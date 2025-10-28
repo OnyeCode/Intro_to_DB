88b96f3cebce3b2f55a7ac02cd842f34f7689a69
Chinemerem Okonkwo <Engrchiok@gmail.com>
2025-10-28 17:21:38 +0100

    Done task 1

---
+++ MySQLServer.py
@@ -0,0 +1,20 @@
+Import mysql.connector
+
+mydb = mysql.connector.connect(
+        host = 'localhost',
+        username = 'root',
+        password = '**********',
+        database = 'test_db'
+        )
+
+mycursor = mydb.cursor()
+
+try:
+    mycursor.execute("CREATE DATABASE alx_book_store")
+except EXCEPTION as e:
+    print(e)
+else:
+    print("Database 'alx_book_store' created successfully!")
+finally:
+    mycursor.close()
+    mydb.close()
